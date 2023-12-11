be59405a35eb3a0564e8326c546f9a396e0becbd
Balazs Cseh <balazs.cseh89@gmail.com>
2023-12-11 17:21:13 +0000

    chore: cleanup, publish as commonjs

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-module.exports = {
-  testMatch: ["**/__tests__/**/*.(ts|tsx|js|jsx)"],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-};
